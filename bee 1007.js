cc866e982fb9eb70c12806f08c4bea9391be841a
André Paiva Garcia <andregarcia0412@gmail.com>
2025-05-15 08:05:33 -0300

    Update bee 1007.js

--- bee 1007.js
+++ bee 1007.js
@@ -3,7 +3,7 @@ var lines = input.split('\n');
 //beecrowd 1007
 var [a,b,c,d] = input.split("\n").map(item => Number(item)) //cria o array "a, b, c, d" e coloca os valores do input ja separados como Number, agilizando o processo (Desestruturação), tambem pode ser lines.map... e o argumento de map pode ser apenas Number
 var diferenca = (a * b) - (c * d)
-console.log(`DIFERENCA = ${diferenca}`) //template literal, é o equivalente ao f string no python
+console.log(`DIFERENCA = ${diferenca}`) //template literal
 
 //para separar valores em "matriz" usar:
 var [a,b,c,d] = lines[0].split(" ").map(Number)
