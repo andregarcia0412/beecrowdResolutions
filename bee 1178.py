b14c03656c3293d22f10ba06dbfa4b02deea650b
André Paiva Garcia <andregarcia0412@gmail.com>
2025-05-19 17:43:47 -0300

    Update bee 1178.py

--- bee 1178.py
+++ bee 1178.py
@@ -1,6 +1,21 @@
+#essa questão precisou ser em python, pois o javascript não teve precisão de ponto flutuante o suficiente para armazenar os valores dessa questão
 x = float(input())
 arr = []
 for i in range(100):
     arr.append(x)
     print(f"N[{i}] = {x:.4f}")
     x /= 2
+
+# código em javascript:
+# var input = require('fs').readFileSync('stdin', 'utf8');
+# var lines = input.split("\n")
+
+# let x = Number(input)
+# let arr = []
+# arr.push(x)
+# let n = x
+# for(var i = 0; i < 100; i++){
+#     console.log(`N[${i}] = ${n.toFixed(4)}`)
+#     arr.push(n)
+#     n = n / 2
+# }
