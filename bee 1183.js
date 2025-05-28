bae8280e0437d60aeb51736e1040628ba532da2a
André Paiva Garcia <andregarcia0412@gmail.com>
2025-05-28 17:51:20 -0300

    Create bee 1183.js

---
+++ bee 1183.js
@@ -0,0 +1,27 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+function gerarMatriz (linha,coluna){
+    let m = []
+    for(var i = 0; i < linha; i++){
+        m[i] = []
+        for (var j = 0; j < coluna; j++){
+            m[i][j] = Number(lines.shift())
+        }
+    }
+    return m
+}
+let operacao = lines.shift().trim()
+let matriz = gerarMatriz(12,12)
+let soma = 0
+
+for(var i = 0; i < 11; i++){
+    for(var j = i + 1; j <= 11; j++){
+        soma += matriz[i][j]
+    }
+}
+if (operacao.toLowerCase() == "s"){
+    console.log(soma.toFixed(1))
+} else if (operacao.toLowerCase() == "m"){
+    console.log((soma/66).toFixed(1))
+}
