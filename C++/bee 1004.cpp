8980e97dcdf01d38be588ca455b6b3b99c378599
André Paiva Garcia <andregarcia0412@gmail.com>
2025-06-15 14:25:51 -0300

    Create bee 1004.cpp

---
+++ C++/bee 1004.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cmath>
+
+int main(){
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int PROD = a * b;
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
