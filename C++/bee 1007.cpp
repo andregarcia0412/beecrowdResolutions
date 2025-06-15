a20165521f28dc02b709fb05a05776d2752e7a09
André Paiva Garcia <andregarcia0412@gmail.com>
2025-06-15 14:59:35 -0300

    Create bee 1007.cpp

---
+++ C++/bee 1007.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cmath>
+
+int main(){
+    int a;
+    int b;
+    int c;
+    int d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    printf("DIFERENCA = %d\n", a * b - c * d);
+
+    return 0;
+}
