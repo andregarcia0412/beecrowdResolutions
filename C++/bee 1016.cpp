5d1acc073064216f9dc54df7953b115237aac0b1
André Paiva Garcia <andregarcia0412@gmail.com>
2025-06-17 02:19:23 -0300

    Create bee 1016.cpp

---
+++ C++/bee 1016.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+    cout << num*2 << " minutos" << endl;
+    return 0;
+}
