54341a300206909bb2af6fba23b420b2235c82bd
André Paiva Garcia <andregarcia0412@gmail.com>
2025-08-13 08:36:34 -0300

    Create bee 2381.cpp

---
+++ C++/bee 2381.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(string arr[], int n){
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-1; j++){
+            if(arr[j] > arr[j+1]){
+                string temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    string arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    bubbleSort(arr, n);
+    cout << arr[k-1] << endl;
+}
