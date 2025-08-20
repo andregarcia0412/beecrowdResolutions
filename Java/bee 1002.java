2b41a4ab46081d1b49d9498ce59c3d29e5616159
André Paiva Garcia <andregarcia0412@gmail.com>
2025-08-19 22:02:26 -0300

    Create bee 1002.java

---
+++ Java/bee 1002.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double pi = 3.14159;
+        double raio = sc.nextDouble();
+        double area = pi * (raio * raio);
+        System.out.printf("A=%.4f\n", area);
+    }
+}
