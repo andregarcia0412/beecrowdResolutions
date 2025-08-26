36a35168a21a296977a1a376e0bbef7e67cd3957
André Paiva Garcia <andregarcia0412@gmail.com>
2025-08-25 22:43:51 -0300

    Create bee 1043.cpp

---
+++ C++/bee 1043.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    float a, b, c;
+    cin >> a >> b >> c;
+    if (a + b > c && a + c > b && b + c > a) {
+        cout << fixed << setprecision(1) << "Perimetro = " << a + b + c << endl;
+    } else {
+        cout << fixed << setprecision(1) << "Area = " << (a + b) * c / 2 << endl;
+    }
+
+        return 0;
+}
+
