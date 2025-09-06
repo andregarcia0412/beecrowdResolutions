b129b07cd9449d03900925418d230d0a8e3a3229
André Paiva Garcia <andregarcia0412@gmail.com>
2025-09-06 12:14:47 -0300

    Create bee 1589.cpp

---
+++ C++/bee 1589.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int r1, r2;
+        cin >> r1 >> r2;
+        cout << r1 + r2 << endl;
+    }
+
+    return 0;
+}
+
