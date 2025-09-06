0461e2d09f1125296b7a6e7e318b61f1b47e6e9e
André Paiva Garcia <andregarcia0412@gmail.com>
2025-09-06 17:37:53 -0300

    Create bee 1014.java

---
+++ Java/bee 1014.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        double y;
+        x = sc.nextInt();
+        y = sc.nextDouble();
+
+        System.out.printf("%.3f km/l\n", x/y);
+    }
+}
