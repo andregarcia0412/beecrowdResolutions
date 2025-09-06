df4b70a2d8e7f278a2fd3353c719e50cfdf3386a
André Paiva Garcia <andregarcia0412@gmail.com>
2025-09-06 17:35:13 -0300

    Create bee 1007.java

---
+++ Java/bee 1007.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, d;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+
+        System.out.println("DIFERENCA = " + (a * b - c * d));
+    }
+}
