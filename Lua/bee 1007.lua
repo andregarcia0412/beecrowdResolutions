51fe2c2cd5ccb122bde306da490ac9b4a33e50b8
André Paiva Garcia <andregarcia0412@gmail.com>
2025-09-07 00:57:34 -0300

    Create bee 1007.lua

---
+++ Lua/bee 1007.lua
@@ -0,0 +1,6 @@
+local a = tonumber(io.read("*n"))
+local b = tonumber(io.read("*n"))
+local c = tonumber(io.read("*n"))
+local d = tonumber(io.read("*n"))
+
+print("DIFERENCA = " .. (a * b - c * d))
