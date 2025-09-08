f33723f41df9815ddb6a147f9093d358c4d4bc9e
André Paiva Garcia <andregarcia0412@gmail.com>
2025-09-07 23:10:14 -0300

    Create bee 1014.c

---
+++ C/bee 1014.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void) {
+
+    int distancia;
+    double combustivel;
+    scanf("%d %lf", &distancia, &combustivel);
+    printf("%.3lf km/l\n",distancia/combustivel);
+
+    return 0;
+}
