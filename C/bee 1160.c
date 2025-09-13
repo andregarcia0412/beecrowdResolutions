e2f342438504f05305e608c2ad5b308b7ce2800b
André Paiva Garcia <andregarcia0412@gmail.com>
2025-09-13 11:26:27 -0300

    Create bee 1160.c

---
+++ C/bee 1160.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+    int t;
+    scanf("%d", &t);
+    for(int i = 0; i < t; i++) {
+        double pa,pb,g1,g2;
+        scanf("%lf %lf %lf %lf", &pa, &pb, &g1, &g2);
+        int qtdAnos = 0;
+
+        while(pa <= pb){
+            pa = floor(pa * (g1/100) + pa);
+            pb = floor(pb * (g2/100) + pb);
+            qtdAnos++;
+            if(qtdAnos > 100){
+                printf("Mais de 1 seculo.\n");
+                break;
+            }
+        }
+        if(qtdAnos <= 100){
+            printf("%d anos.\n", qtdAnos);
+        }
+
+    }
+
+    return 0;
+}
