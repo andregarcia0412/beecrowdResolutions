b9a4a2014661979a5ee4dc9b4f96e47b284fbe15
André Paiva Garcia <andregarcia0412@gmail.com>
2026-02-04 12:51:12 -0300

    Create bee 1003.kt

---
+++ Kotlin/bee 1003.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+import kotlin.math.pow
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val a = br.readLine().toInt()
+    val b = br.readLine().toInt()
+
+    println("SOMA = ${a+b}")
+
+}
