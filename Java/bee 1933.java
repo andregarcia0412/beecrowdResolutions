fc845bdd68071a6c6ffb811057441445549c9081
André Paiva Garcia <andregarcia0412@gmail.com>
2026-03-13 12:22:28 -0300

    Create bee 1933.java

---
+++ Java/bee 1933.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(maior(a,b));
+    }
+    public static int maior(int a, int b) {
+        if(a > b) {
+            return a;
+        }
+
+        return b;
+    }
+}
