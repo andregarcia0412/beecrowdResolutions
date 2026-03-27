1dd7d3eec1ac1514674ffdaaba70b3b5ab995449
André Paiva Garcia <andregarcia0412@gmail.com>
2026-03-27 12:57:13 -0300

    Create bee 2544.cpp

---
+++ C++/bee 2544.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    while(cin >> n) {
+        cout << log(n) / log(2) << endl; 
+    }
+    
+    return 0;
+}
